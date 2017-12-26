b6bc2be6e069896b34c64b5099d2263bb42c6919
xia5081 <34758498+xia5081@users.noreply.github.com>
2017-12-26 15:33:09 +0800

    夏凌猛
    上传时间：2017-12-26
    上传内容：登录失焦

--- socialTourism/public/static/js/login.js
+++ socialTourism/public/static/js/login.js
@@ -38,7 +38,77 @@ var app2 = new Vue({
                     }
                 }
             })
+        },
+
+        tell:function(){
+            if(this.phone==''){
+                $('#telltps').css('color','red');
+                $('#telltps').css('font-size','20px');
+                $("#telltps").html("× 输入不能为空")
+            }else if(this.phone.length<11){
+                $('#telltps').css('color','red');
+                $('#telltps').css('font-size','20px');
+                $("#telltps").html("× 电话号码长度小于11位数")
+            }else if(this.phone.length>11){
+                $('#telltps').css('color','red');
+                $('#telltps').css('font-size','20px');
+                $("#telltps").html("× 电话号码长度大于11位数")
+            }else if(!checkMobile(this.phone)){
+                $('#telltps').css('color','red');
+                $('#telltps').css('font-size','20px');
+                $("#telltps").html("× 电话号码格式不正确")
+            }else {
+                $('#telltps').css('color','green');
+                $('#telltps').css('font-size','20px');
+                $("#telltps").html("√")
+            }
+        },
+
+
+        doSomethingElse:function(){
+            if(this.pwd==''){
+                $('#pwdtps').css('color','red');
+                $('#pwdtps').css('font-size','20px');
+                $("#pwdtps").html("× 输入不能为空")
+            }else if(this.pwd.length<6){
+                $('#pwdtps').css('color','red');
+                $('#pwdtps').css('font-size','20px');
+                $("#pwdtps").html("× 输入长度小于6位")
+            }else if(this.pwd.length>20){
+                $('#pwdtps').css('color','red');
+                $('#pwdtps').css('font-size','20px');
+                $("#pwdtps").html("× 输入长度大于20位")
+            }else {
+                $('#pwdtps').css('color','green');
+                $('#pwdtps').css('font-size','20px');
+                $("#pwdtps").html("√")
+            }
+        },
+
+
+        codein:function(){
+            if(this.code==''){
+                $('#codeon').css('color','red');
+                $('#codeon').css('font-size','20px');
+                $("#codeon").html("× 输入不能为空")
+            }else {
+                $('#codeon').css('color','green');
+                $('#codeon').css('font-size','20px');
+                $("#codeon").html("√")
+            }
         }
     },
 
+
 })
+
+function checkMobile(s){
+    var regu =/^[1][3/5/8][0-9]{9}$/;
+    var re = new RegExp(regu);
+    if (re.test(s)) {
+        return true;
+    }else{
+        return false;
+    }
+}
+
