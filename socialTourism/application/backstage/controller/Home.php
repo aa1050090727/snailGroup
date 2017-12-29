827bb97977e17766edc154ccedcd3f7a0e82c6a2
aa1050090727 <34758887+aa1050090727@users.noreply.github.com>
2017-12-29 10:07:36 +0800

    陈凌峰
    上传时间：2017.12.29
    后台

--- socialTourism/application/backstage/controller/Home.php
+++ socialTourism/application/backstage/controller/Home.php
@@ -10,13 +10,30 @@ namespace app\backstage\controller;
 
 
 use think\Controller;
+use think\Db;
+use think\Session;
 
 class Home extends Controller
 {
     public function home(){
+
         return $this->fetch();
     }
     public function main(){
         return $this->fetch();
+    }
+    public function getthisuser(){
+        $backstageUser = Session::get("backstageUser");
+        if(!empty($backstageUser)){
+            $retArr = Db::table('b_rm')->alias('a')->join('b_menu b','a.b_rm_menu_id=b.b_menu_id')->where("a.b_rm_role_id",$backstageUser["b_user_role_id"])->select();
+            return json(['code'=>10000,'msg'=>'','data'=>$retArr,'url'=>'']);
+        }else{
+            return json(['code'=>10001,'msg'=>'','data'=>'','url'=>url('backstage/Index/index')]);
+        }
+//        var_dump($backstageUser);
+        //echo json_encode($backstageUser);
+    }
+    public function build(){
+
     }
 }
