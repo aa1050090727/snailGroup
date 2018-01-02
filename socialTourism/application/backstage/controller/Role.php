539a89052bfae266b561391755944c976befcd0f
aa1050090727 <34758887+aa1050090727@users.noreply.github.com>
2018-01-02 14:10:43 +0800

    陈凌峰
    上传时间：2018.1.2

--- socialTourism/application/backstage/controller/Role.php
+++ socialTourism/application/backstage/controller/Role.php
@@ -10,11 +10,23 @@ namespace app\backstage\controller;
 
 
 use think\Controller;
+use think\Db;
 
 class Role extends Controller
 {
     public function role(){
         return $this->fetch();
+    }
+    public function getRole(){
+        $page = $_GET["page"];
+        $limit = $_GET["limit"];
+//        var_dump($page);
+//        var_dump($limit);
+        $resArr = Db::table("b_role")->limit("0,10")->select();
+        $resCount = Db::table("b_role")->count();
+         //var_dump($resArr);
+        $resArrJson = '{"code": 0,"msg": "","count": '.$resCount.',"data": '.json_encode($resArr,JSON_UNESCAPED_UNICODE).'}';
+        return json_decode($resArrJson);
     }
 
 }
---
+++ socialTourism/public/static/js/backstage/role.js
@@ -0,0 +1,7 @@
+/**
+ * Created by 陈凌峰 on 2017/12/29.
+ */
+
+$("#searchBut").click(function () {
+    var roleSearch = $("#roleSearch").val();
+});
