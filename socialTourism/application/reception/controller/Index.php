5bc79314481a1c4549d256d388db933fc8550722
aa1050090727 <34758887+aa1050090727@users.noreply.github.com>
2018-01-28 22:43:37 +0800

    陈凌峰
    上传时间：2018.01.28

--- socialTourism/application/reception/controller/Index.php
+++ socialTourism/application/reception/controller/Index.php
@@ -3,13 +3,10 @@ namespace app\reception\controller;
 
 use think\Controller;
 use think\Db;
-<<<<<<< HEAD
 use think\Session;
 use think\Response;
 use think\Request;
-=======
 use think\Cache;
->>>>>>> a233b05f035b10640663db6f7ba95c36972201df
 //首页控制器
 class Index extends Controller
 {
@@ -17,7 +14,6 @@ class Index extends Controller
     public function index(){
         return $this->fetch();
     }
-<<<<<<< HEAD
     /*热门游记显示*/
     public function hotTravel(){
         $nowpage=input('param.nowPage');
@@ -44,7 +40,6 @@ class Index extends Controller
         }
         return json(['nowPage'=>$nowpage,'allPage'=>$allPage,'hotTravel'=>$res]);
     }
-=======
 	public function setredis(){
 		date_default_timezone_set("PRC");
         $timept=date("Y-m-d 12:00:00",time());
@@ -62,7 +57,5 @@ class Index extends Controller
 		// $list = $redis->lrange('f_science', 0, -1);
 		//$res = $redis->lPop('f_science');
     }
-
->>>>>>> a233b05f035b10640663db6f7ba95c36972201df
 }
 
